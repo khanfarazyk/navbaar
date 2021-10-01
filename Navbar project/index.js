151925c8d192fb45ef4399938a6f311f0b573527
khanfarazyk <81009562+khanfarazyk@users.noreply.github.com>
2021-10-01 18:23:23 +0500

    Add files via upload

---
+++ Navbar project/index.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+
+toggleBtn.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active');
+})
